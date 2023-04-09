b2955a3f228d06d9e239ecca8112363cd961f6c5
Vlad Soloshuk <vladsoloshuk@gmail.com>
2023-04-09 22:11:24 +0200

    03-PosterShopProject

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,223 +1,177 @@
-const ATTACK_VALUE = 10;
-const STRONG_ATTACK_VALUE = 17;
-const MONSTER_ATTACK_VALUE = 14;
-const HEAL_VALUE = 20;
-
-const MODE_ATTACK = 'ATTACK';
-const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
-const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
-const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
-const LOG_EVENT_MOSTER_ATTACK = 'MOSTER__ATTACK';
-const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
-const LOG_EVENT_GAME_OVER = 'GAME_OVER';
-
-const enteredValue = prompt ('Enter health amount of you and the monster', '100');
-
-let chosenMaxLife = parseInt(enteredValue);
-let battleLog = [];
-let lastLogEntry;
-
-if(isNaN(chosenMaxLife || chosenMaxLife <= 0)) {
-  chosenMaxLife = 100;
-}
-let currentMonsterHealth = chosenMaxLife;
-let currentPlayerHealth = chosenMaxLife;
-let hasBonusLife = true;
-
-adjustHealthBars (chosenMaxLife);
-
-function writeToLog(event, value, monsterHealth, playerHealth) {
-  let logEntry = {
-    event: event,
-      value: value,
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-  };
-
-  switch (event) {
-    case LOG_EVENT_PLAYER_ATTACK:
-      logEntry.target = 'MOSTER';
-      break;
-    case LOG_EVENT_PLAYER_STRONG_ATTACK:
-      logEntry = {
-        event: event,
-        value: value,
-        target: 'MOSTER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
-      break;
-    case LOG_EVENT_PLAYER_HEAL:
-      logEntry = {
-        event: event,
-        value: value,
-        target: 'PLAYER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
-      break;
-    case LOG_EVENT_MOSTER_ATTACK:
-      logEntry = {
-        event: event,
-        value: value,
-        target: 'PLAYER',
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
-      break;
-    case LOG_EVENT_GAME_OVER:
-      logEntry = {
-        event: event,
-        value: value,
-        finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
-      };
-      break;
-      default: logEntry = {};
-  }
-
-  /*if(event === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MOSTER';
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: 'MOSTER',
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: 'PLAYER',
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event === LOG_EVENT_MOSTER_ATTACK) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: 'PLAYER',
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event = LOG_EVENT_GAME_OVER) {
-    logEntry = {
-      event: event,
-      value: value,
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  }*/
-  battleLog.push(logEntry);
-}
-
-function reset() {
-  alert("Game has been reseted.");
-  currentMonsterHealth = chosenMaxLife;
-  currentPlayerHealth = chosenMaxLife;
-  resetGame(chosenMaxLife);
-}
-
-function endRound() {
-  const initalPlayerHealth = currentPlayerHealth;
-  const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= monsterDamage;
-  writeToLog(LOG_EVENT_MOSTER_ATTACK, monsterDamage, currentMonsterHealth, currentPlayerHealth);
-
-   if (currentPlayerHealth <= 0 && hasBonusLife) {
-    hasBonusLife = false;
-    removeBonusLife();
-    currentPlayerHealth = initalPlayerHealth;
-    setPlayerHealth(currentPlayerHealth);
-    alert('You are lucky today. You have bonus life!');
-   }
-
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert ('Monster is dead. You won!');
-    writeToLog(LOG_EVENT_GAME_OVER, 'PLAYER WON', currentMonsterHealth, currentPlayerHealth);
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert ('That was the great battle, but you lost!');
-    writeToLog(LOG_EVENT_GAME_OVER, 'MOSTER WON', currentMonsterHealth, currentPlayerHealth);
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0 ) {
-    alert ("It's a draw!");
-    writeToLog(LOG_EVENT_GAME_OVER, 'DRAW', currentMonsterHealth, currentPlayerHealth);
-  }
-
-  if (currentMonsterHealth < 0 || currentPlayerHealth < 0)
-  {
-    reset();
-  }
-}
-
-function attackMonster(mode) {
-  //using ternary expression
-  const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
-  const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
-  //using if statement
-  /*if (mode === MODE_ATTACK) {
-    maxDamage = ATTACK_VALUE;
-    logEvent = LOG_EVENT_PLAYER_ATTACK;
-   } else if (mode === MODE_STRONG_ATTACK) {
-    maxDamage = STRONG_ATTACK_VALUE;
-    logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-   }*/
-   const damage = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= damage;
-  writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
-  endRound();
-}
-
-function attackHandler() {
-  attackMonster(MODE_ATTACK);
-}
-
-function strongAttackHandler() {
-  attackMonster(MODE_STRONG_ATTACK);
-}
-
-function healPlayerHandler() {
-  let healValue;
-  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
-    alert ("You can't heal to more than your inital health.");
-    healValue = chosenMaxLife - currentPlayerHealth;
-  } else {
-    healValue = HEAL_VALUE;
-  }
-  increasePlayerHealth(healValue);
-  currentPlayerHealth += healValue;
-  writeToLog(LOG_EVENT_PLAYER_HEAL, healValue, currentMonsterHealth, currentPlayerHealth);
-  endRound();
-}
-
-function printLogHandler() {
-  /*for (let i = 0; i < battleLog.length; i++) {
-    console.log(battleLog[i]);
-  }*/
-  let j = 0;
-  do {
-    console.log('-----');
-    j++;
-  }
-  while (j < 3);
-
-  let i = 0;
-  for (const logEntry of battleLog) {
-    if ((!lastLogEntry && lastLogEntry !== 0) || lastLogEntry < i) {
-      console.log(`#${i}`);
-      for (const key in logEntry) {
-        console.log(`${key} => ${logEntry[key]}`);
-      }
-      lastLogEntry = i;
-      break;
+class Product {
+    /*title = 'DEFAULT';
+    imageUrl = ''
+    description = '';
+    prcie = 0;*/
+  
+    constructor(title, image, desc, price) {
+      this.title = title;
+      this.imageUrl = image;
+      this.description = desc;
+      this.price = price;
     }
-    i++;
   }
-
-}
-
-healBtn.addEventListener('click', healPlayerHandler);
-attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click', strongAttackHandler);
-logBtn.addEventListener('click', printLogHandler);
+  
+  class ElementAttribute {
+    constructor(attrName, attrValue) {
+      this.name = attrName;
+      this.value = attrValue;
+    }
+  }
+  
+  class Component {
+    constructor(renderHookId, shouldRender = true) {
+      this.hookId = renderHookId;
+      if(shouldRender) {
+        this.render();
+      }
+    }
+  
+    render() {}
+  
+    createRootElement(tag, cssClasses, attributes) {
+      const rootElement = document.createElement(tag);
+      if (cssClasses) {
+        rootElement.className = cssClasses;
+      }
+      if (attributes && attributes.length > 0) {
+        for (const attr of attributes) {
+          rootElement.setAttribute(attr.name, attr.value);
+        }
+      }
+      document.getElementById(this.hookId).append(rootElement);
+      return rootElement;
+    }
+  }
+  
+  class ShoppingCart extends Component {
+    items = [];
+  
+    set cartItems(value) {
+      this.items = value;
+      this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+  
+    get totalAmount() {
+      const sum = this.items.reduce((prevValue, currentItem) => {
+        return prevValue + currentItem.price;
+      }, 0);
+      return sum;
+    }
+  
+    constructor(renderHookId) {
+      super(renderHookId);
+    }
+  
+    addProduct(product) {
+      const updatedItems = [...this.items];
+      updatedItems.push(product);
+      this.cartItems = updatedItems;
+    }
+  
+    orderProducts() {
+      console.log('Ordering: ' + this.items);
+    }
+  
+    render() {
+      const cartEl = this.createRootElement('section', 'cart',);
+      cartEl.innerHTML = `
+        <h2>Total: \$${0}</h2>
+        <button>Order now!</button>
+        `;
+        const orderButton = cartEl.querySelector('button');
+        orderButton.addEventListener('click', this.orderProducts.bind(this));
+        this.totalOutput = cartEl.querySelector('h2');
+    }
+  }
+  
+  class ProductItem extends Component {
+    constructor(product, renderHookId) {
+      super(renderHookId, false);
+      this.product = product;
+      this.render();
+    }
+  
+    addToCart() {
+      App.addProductToCart(this.product);
+    }
+  
+    render() {
+      const prodEl = this.createRootElement('li', 'product-item',);
+        prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}"> 
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>`;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
+    }
+  }
+  
+  class ProductList extends Component {
+    #products = [];
+  
+    constructor(renderHookId) {
+      super(renderHookId);
+      this.render();
+      this.fetchProducts();
+    }
+  
+    fetchProducts() {
+      this.#products = [
+        new Product('An anime poster 1', 'https://i.etsystatic.com/27390177/r/il/542f02/4356010459/il_1140xN.4356010459_5ioo.jpg', 'A chainsaw man poster.', 9.99),
+        new Product('An anime poster 2', 'https://i.etsystatic.com/27390177/r/il/e52662/3917953368/il_1140xN.3917953368_2pab.jpg', 'A spy x family poster.', 10.99),
+        new Product('An anime poster 3', 'https://i.etsystatic.com/27390177/r/il/e33247/3596426645/il_1140xN.3596426645_97td.jpg', 'A demon slayer poster.', 10.99),
+        new Product('An anime poster 4', 'https://i.etsystatic.com/27390177/r/il/c71578/3596426075/il_1140xN.3596426075_srbv.jpg', 'A my hero academia poster.', 15.99),
+        new Product('An anime poster 5', 'https://i.etsystatic.com/27390177/r/il/f8f234/4308616594/il_1140xN.4308616594_8w2l.jpg', 'A jujutsu kaisen poster.', 12.99),
+        new Product('An anime poster 6', 'https://i.etsystatic.com/27390177/r/il/f11a0e/4308617006/il_1140xN.4308617006_hmve.jpg', 'A death note poster.', 16.99),
+        new Product('An anime poster 7', 'https://i.etsystatic.com/27390177/r/il/5e7977/4308616992/il_1140xN.4308616992_rn4h.jpg', 'A banana fish poster.', 15.99)
+      ];
+      this.renderProducts();
+    }
+  
+    renderProducts() {
+      for (const prod of this.#products) {
+        const productItem = new ProductItem(prod, 'prod-list');
+      }
+    }
+  
+    render() {
+      const prodList = this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')]);
+      if (this.product && this.#products.length > 0) {
+        this.renderProducts();
+      }
+    }
+  }
+  
+  class Shop {
+  
+    constructor() {
+      this.render();
+    }
+  
+    render() {
+      this.cart = new ShoppingCart('app');
+      new ProductList('app');
+    }
+  }
+  
+  class App {
+    static cart;
+  
+    static init() {
+      const shop = new Shop();
+      this.cart = shop.cart;
+    }
+  
+    static addProductToCart(product) {
+      this.cart.addProduct(product);
+    }
+  }
+  
+  App.init();
